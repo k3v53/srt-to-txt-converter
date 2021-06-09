dc8d149127a2e06083f7ceb2ee71e6b0330d81a6
k3v53 <25158905+k3v53@users.noreply.github.com>
2021-06-08 21:53:05 -0300

    Simple optimization

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let fs = require("fs");
 let srtfile = fs.readFileSync("input.srt", "utf-8");
-let retvar = "";
 let srtvar = srtfile.split("\r\n");
-for (let i = 2; i < srtvar.length; i+=4) {
+let retvar = ""+srtvar[2];
+for (let i = 6; i < srtvar.length; i+=4) {
   const el = srtvar[i];
-  retvar += "\r\n"+el;
+  if(el != ""){
+    retvar += "\r\n"+el;
+  }
 }
 fs.writeFileSync("output.txt", retvar);
